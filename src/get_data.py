08efcbd7f731ec90c66d607e4859b972d8a19c8d
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-02-13 00:55:50 +0100

    Data extraction updated to be 4-year batched, and respect API request limit. First run works, but haven't downloaded yet all dataset.
    Better to run the download on a GPU since these requests take hours to process on the ERA5 website. Hence, the device needs to be running for a good while.

--- src/get_data.py
+++ src/get_data.py
@@ -63,7 +63,7 @@ mountain_areas = {
     "Shishapangma": [29.51, 84.16, 26.51, 87.16],
 }
 
-request = {
+base_request = {
     "product_type": ["reanalysis"],
     "variable": [
         "10m_u_component_of_wind",
@@ -123,37 +123,38 @@ request = {
         "temperature_of_snow_layer",
         "total_column_snow_water"
     ],
-    "year": [
-        "1940", "1941", "1942",
-        "1943", "1944", "1945",
-        "1946", "1947", "1948",
-        "1949", "1950", "1951",
-        "1952", "1953", "1954",
-        "1955", "1956", "1957",
-        "1958", "1959", "1960",
-        "1961", "1962", "1963",
-        "1964", "1965", "1966",
-        "1967", "1968", "1969",
-        "1970", "1971", "1972",
-        "1973", "1974", "1975",
-        "1976", "1977", "1978",
-        "1979", "1980", "1981",
-        "1982", "1983", "1984",
-        "1985", "1986", "1987",
-        "1988", "1989", "1990",
-        "1991", "1992", "1993",
-        "1994", "1995", "1996",
-        "1997", "1998", "1999",
-        "2000", "2001", "2002",
-        "2003", "2004", "2005",
-        "2006", "2007", "2008",
-        "2009", "2010", "2011",
-        "2012", "2013", "2014",
-        "2015", "2016", "2017",
-        "2018", "2019", "2020",
-        "2021", "2022", "2023",
-        "2024", "2025"
-    ],
+    # "year": [
+    #     "1940", "1941", "1942",
+    #     "1943", "1944", "1945",
+    #     "1946", "1947", "1948",
+    #     "1949", "1950", "1951",
+    #     "1952", "1953", "1954",
+    #     "1955", "1956", "1957",
+    #     "1958", "1959", "1960",
+    #     "1961", "1962", "1963",
+    #     "1964", "1965", "1966",
+    #     "1967", "1968", "1969",
+    #     "1970", "1971", "1972",
+    #     "1973", "1974", "1975",
+    #     "1976", "1977", "1978",
+    #     "1979", "1980", "1981",
+    #     "1982", "1983", "1984",
+    #     "1985", "1986", "1987",
+    #     "1988", "1989", "1990",
+    #     "1991", "1992", "1993",
+    #     "1994", "1995", "1996",
+    #     "1997", "1998", "1999",
+    #     "2000", "2001", "2002",
+    #     "2003", "2004", "2005",
+    #     "2006", "2007", "2008",
+    #     "2009", "2010", "2011",
+    #     "2012", "2013", "2014",
+    #     "2015", "2016", "2017",
+    #     "2018", "2019", "2020",
+    #     "2021", "2022", "2023",
+    #     "2024"
+    # ],
+    "year": [],
     "month": [
         "01", "02", "03",
         "04", "05", "06",
@@ -179,21 +180,35 @@ request = {
     "area": []
 }
 
-er5_data_path = Path('data/era5_data')
+era5_data_path = Path('data/era5_data')
+era5_data_path.mkdir(parents=True, exist_ok=True)
 total_size = 0
+
+# 5-year consecutive batches due to API request limits
+year_batches = []
+for year in range(2024, 1940, -4):
+    batch = [year, year - 1, year - 2, year - 3]
+    year_batches.append(batch)
+
+year_batches.append([1940]) # we have 85 values where 85 % 4 = 1, so the last value will be in its own batch
+# print(year_batches)
 
 # Documentation to run the request: https://cds.climate.copernicus.eu/how-to-api
 for mountain, area in mountain_areas.items():
 
     print(f"Downloading data for {mountain}...")
-    mountain_folder = er5_data_path / mountain
+    mountain_folder = era5_data_path / mountain
     mountain_folder.mkdir(parents=True, exist_ok=True)
-    file_path = mountain_folder / f"{mountain}.grib"
 
-    request["area"] = area
-    client.retrieve(era5_dataset, request, str(file_path))
+    for year_batch in year_batches:
 
-    total_size += file_path.stat().st_size if file_path.exists() else 0
+        batch_start, batch_end = year_batch[0], year_batch[-1]
+        file_path = mountain_folder / f"{mountain}_{batch_start}_{batch_end}.grib"
+        request = base_request.copy()
+        request["year"] = [str(y) for y in year_batch]
+        request["area"] = area
+        client.retrieve(era5_dataset, request, str(file_path))
+        total_size += file_path.stat().st_size if file_path.exists() else 0
 
     print(f"Data for {mountain} saved as {file_path}.")
 
