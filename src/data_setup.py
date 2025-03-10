d52010d14c0de2bf868ec3b4eb2a3d8ae38da46d
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-03-10 18:27:00 +0100

    Makalu Batch Done

--- src/data_setup.py
+++ src/data_setup.py
@@ -1,7 +1,7 @@
 import pygrib
 from pathlib import Path
 
-era5_data_path = Path("data/era5_data/Everest/Everest-2020-2024")
+era5_data_path = Path("data/era5_data/Makalu/Makalu-1960-1964")
 
 # Open the GRIB file using pygrib
 with pygrib.open(era5_data_path) as grbs:
