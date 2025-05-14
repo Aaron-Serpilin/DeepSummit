ec089e3acad8c51744a4f68ab823b485f64cac4e
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-05-14 20:42:56 +0200

    Bash .sh file for das5 shell added

---
+++ run_main_gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -C RTX2080Ti
+#SBATCH -p proq
+#SBATCH --gres=gpu:1
+
+module load cuda10.2/toolkit
+
+cd /var/scratch/ase347/DeepSummit
+/var/scratch/ase347/anaconda3/envs/deepsummit/bin/python main.py
