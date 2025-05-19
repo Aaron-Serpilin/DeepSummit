fb3696ad3db753ffd74e3f4d752ce269a6dcfb74
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-05-20 00:52:56 +0200

    Refactored WeatherDataset. Added intra sample masks for smaller window contexts.

--- src/met_transformer/met_utils.py
+++ src/met_transformer/met_utils.py
@@ -12,6 +12,7 @@ class WeatherDataset (Dataset):
     def __init__(self,
                  csv_file: Path,
                  target_column: str,
+                 metadata_cols: List[str] = None,
                  continuous_mean_std: List[Tuple[float, float]] = None,
                  transform: transforms.Compose = None
                  ):
@@ -27,46 +28,66 @@ class WeatherDataset (Dataset):
         """
         
         self.data = pd.read_csv(csv_file, parse_dates=True)
+
+        # Params
         self.target_column = target_column
         self.transform = transform
 
-        metadata_cols = ["PEAKID", "parent_peakid", "event_date"]
-        excluded_columns = metadata_cols + [target_column]
+        if metadata_cols is None:
+            metadata_cols = ["PEAKID", "parent_peakid", "event_date"]
+
+        excluded_columns = set(metadata_cols + [target_column])
+
+        # Features DataFrame and column list
         self.feature_df = self.data.drop(columns=excluded_columns) # DataFrame containing all of the values for every flattened feature column
         self.num_features = len(self.feature_df)
-        self.feature_cols = [col for col in self.feature_df if col not in excluded_columns] # flattened column names with the the feature and time offset
+        self.feature_cols = list(self.feature_df.columns) # flattened column names with the the feature and time offset
 
+        # Parsing base feature names and day offsets
         pattern = re.compile(r"^(?P<feat>.+)_t-(?P<off>\d+)$")
         features = set()
         offsets = set()
-
+      
         for column in self.feature_df:
-            feature = pattern.match(column)
 
+            feature = pattern.match(column)
             if not feature:
                 raise ValueError(f"Column {column!r} doesn't match '<feat>_t-<off>' format")
             
             features.add(feature.group("feat"))
             offsets.add(int(feature.group("off")))
 
-        self.features = features # flattened column names without time offsets
+        # Metadata
+        self.base_features = sorted(features) # flattened column names without time offsets
         self.offsets = sorted(offsets)
         self.num_days = len(self.offsets)
-        self.num_feats_per_day = len(self.features)
+        self.num_feats_per_day = len(self.base_features)
+
+        # Stats calculation for normalization
+        stats = self.feature_df.agg(["mean", "std"])
+        self.means = stats.loc["mean"].values.astype(np.float32)
+        self.stds = stats.loc["std"].values.astype(np.float32)
+        self.stds[self.stds == 0] = 1.0 # we do this to avoid division by 0 problems
 
         # Mask to signal priorities/ignored features
-        self.mask = np.ones(self.feature_df.shape, dtype=np.int64)
+        self.mask = np.ones(self.num_days, dtype=np.int64)
 
         # [ cls ] token
         self.cls = np.array([1], dtype=np.int64)
 
-        # Stats calculation for normalization
-        stats = self.data[self.feature_cols].agg(["mean", "std"])
-        self.means = stats.loc["mean"].values.astype(np.float32)
-        self.stds = stats.loc["std"].values.astype(np.float32)
+        # Intra-sample masks for multiple smaller views of the context days
+        seq_len = 1 + self.num_days # we add 1 due to the [ cls ] token
+        self.full_mask = np.concatenate([self.cls, self.mask])
 
-        # print(f"means: {len(self.means)}\n")
-        # print(f"stds: {len(self.stds)}\n")
+        self.window_masks = np.stack([
+            np.concatenate([
+                self.cls,
+                np.ones(intra_context, dtype=np.int64),
+                np.zeros(self.num_days - intra_context, dtype=np.int64)
+            ])
+
+            for intra_context in range(1, seq_len)
+        ], axis=0) 
 
         ### Later add another mask for pressure and most relevant features according to other research papers
 
@@ -81,6 +102,7 @@ class WeatherDataset (Dataset):
         flat = row[self.feature_cols].values.astype(np.float32)
 
         # Normalization
+        print(f"means is {self.means}\nstds is {self.stds}\n")
         flat = (flat - self.means) / self.stds
         # Reshaping into (days, feats)
         X_days = flat.reshape(self.num_days, self.num_feats_per_day)
@@ -98,11 +120,17 @@ class WeatherDataset (Dataset):
         if self.transform:
             X, mask, y = self.transform(X, mask, y)
 
+        # Intra sample mask
+        window_masks = self.window_masks
+
         X_tensor = torch.tensor(X)
         mask_tensor = torch.tensor(mask, dtype=torch.long)
         target_tensor = torch.tensor(y)
+        window_mask_tensor = torch.tensor(window_masks)
 
-        return X_tensor, mask_tensor, target_tensor
+        return X_tensor, mask_tensor, target_tensor, window_mask_tensor
 
 test_path = Path("data/era5_data/era5_data.csv")
-test = WeatherDataset(test_path, 'Target', None)
+test = WeatherDataset(csv_file=test_path, target_column='Target')
+X, mask, y, window_mask = test[5]
+print(f"X is {X}\nmask is: {mask}\ny is: {y}\nwindow_mask is: {window_mask}\n")
