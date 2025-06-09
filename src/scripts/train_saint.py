02ac857b74a88e5f31090a7f89ee6e0c6e7a3abd
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-06-09 22:44:05 +0200

    Updated train_saint.py for experiment tracking on lr=1e-2 and saint depth of 4

--- src/scripts/train_saint.py
+++ src/scripts/train_saint.py
@@ -183,7 +183,7 @@ saint = SAINT(
     num_continuous = len(continuous_columns),                
     dim = 32,                           
     dim_out = 1,                       
-    depth = 6,                       
+    depth = 4,                       
     heads = 8,  
     num_special_tokens=1,                      
     attn_dropout = 0.1,             
@@ -198,7 +198,7 @@ saint = SAINT(
 saint.to(device)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(saint.parameters(),lr=0.1, betas=(0.9, 0.999), weight_decay=0.01)
+optimizer = torch.optim.AdamW(saint.parameters(),lr=0.01, betas=(0.9, 0.999), weight_decay=0.01)
 
 saint_results = train(model=saint,
                 train_dataloader=tabular_train_dataloader,
@@ -209,10 +209,10 @@ saint_results = train(model=saint,
                 epochs=50,
                 writer=create_writer(experiment_name="saint_run",
                                     model_name="saint",
-                                    extra="50_epochs_lr_1e-1"))
+                                    extra="50_epochs_lr_1e-2_depth_4"))
 
 plot_loss_curves(saint_results)
 
 save_model(saint,
           "/var/scratch/ase347/DeepSummit/checkpoints",
-          "saint_epoch50_lr_1e-1.pth")
+          "saint_epoch50_lr_1e-2_depth_4.pth")
