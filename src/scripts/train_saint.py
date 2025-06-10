0b547904b485cc520f3de72979302486d65c576d
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-06-11 01:26:48 +0200

    Updated train_saint.py for experiment tracking on lr=1e-5, saint depth = 3, dropout = 0.5

--- src/scripts/train_saint.py
+++ src/scripts/train_saint.py
@@ -183,11 +183,11 @@ saint = SAINT(
     num_continuous = len(continuous_columns),                
     dim = 32,                           
     dim_out = 1,                       
-    depth = 2,                       
+    depth = 3,                       
     heads = 8,  
     num_special_tokens=1,                      
-    attn_dropout = 0.25,             
-    ff_dropout = 0.25,                  
+    attn_dropout = 0.5,             
+    ff_dropout = 0.5,                  
     mlp_hidden_mults = (4, 2),       
     cont_embeddings = 'MLP',
     attentiontype = 'colrow',
@@ -208,10 +208,10 @@ saint_results = train(model=saint,
                 loss_fn=loss_fn,
                 epochs=50,
                 writer=create_writer(experiment_name="saint_runs",
-                                    extra="epochs_50_lr_1e-5_depth_2_dropout_25e-2"))
+                                    extra="epochs_50_lr_1e-5_depth_3_dropout_5e-1"))
 
 plot_loss_curves(saint_results)
 
 save_model(saint,
           "/var/scratch/ase347/DeepSummit/checkpoints",
-          "saint_epochs_50_lr_1e-5_depth_2_dropout_25e-2.pth")
+          "saint_epochs_50_lr_1e-5_depth_3_dropout_5e-1.pth")
