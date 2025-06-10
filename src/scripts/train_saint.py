36ace545a13f00f7470ad00c5cad5a625c67e2a9
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-06-10 22:14:39 +0200

    Updated train_saint.py for experiment tracking on lr=5e-4, saint depth = 3, dropout = 0.25

--- src/scripts/train_saint.py
+++ src/scripts/train_saint.py
@@ -183,11 +183,11 @@ saint = SAINT(
     num_continuous = len(continuous_columns),                
     dim = 32,                           
     dim_out = 1,                       
-    depth = 4,                       
+    depth = 3,                       
     heads = 8,  
     num_special_tokens=1,                      
-    attn_dropout = 0.1,             
-    ff_dropout = 0.1,                  
+    attn_dropout = 0.25,             
+    ff_dropout = 0.25,                  
     mlp_hidden_mults = (4, 2),       
     cont_embeddings = 'MLP',
     attentiontype = 'colrow',
@@ -198,7 +198,7 @@ saint = SAINT(
 saint.to(device)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(saint.parameters(),lr=0.0001, betas=(0.9, 0.999), weight_decay=0.01)
+optimizer = torch.optim.AdamW(saint.parameters(),lr=0.0005, betas=(0.9, 0.999), weight_decay=0.01)
 
 saint_results = train(model=saint,
                 train_dataloader=tabular_train_dataloader,
@@ -208,10 +208,10 @@ saint_results = train(model=saint,
                 loss_fn=loss_fn,
                 epochs=50,
                 writer=create_writer(experiment_name="saint_runs",
-                                    extra="epochs_50_lr_1e-4_depth_4"))
+                                    extra="epochs_50_lr_5e-4_depth_3_dropout_25e-2"))
 
 plot_loss_curves(saint_results)
 
 save_model(saint,
           "/var/scratch/ase347/DeepSummit/checkpoints",
-          "saint_epochs_50_lr_1e-4_depth_4.pth")
+          "saint_epochs_50_lr_5e-4_depth_3_dropout_25e-2.pth")
