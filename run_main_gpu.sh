0a9ce0945f07c2ba0f045c80126889f7a4551be0
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-06-10 17:20:58 +0200

    Updated run_main_gpu.sh to point stdout and stderr into runs/slurm_logs

--- run_main_gpu.sh
+++ run_main_gpu.sh
@@ -6,6 +6,9 @@
 #SBATCH -p proq
 #SBATCH --gres=gpu:1
 
+#SBATCH --output=/var/scratch/ase347/DeepSummit/runs/slurm_logs/%x_%j.out
+#SBATCH --error=/var/scratch/ase347/DeepSummit/runs/slurm_logs/%x_%j.err
+
 module load cuda10.1/toolkit/10.1.243
 
 cd /var/scratch/ase347/DeepSummit
