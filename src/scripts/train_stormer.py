af642a1dfb82c8e2257c5c242adb83d940b0ca5f
Aaron-Serpilin <serpilin.aaron@gmail.com>
2025-06-17 15:48:02 +0200

    Updated train_stormer.py for experiment tracking with hidden_size = 612, lr = 1e-5, depth = 12

--- src/scripts/train_stormer.py
+++ src/scripts/train_stormer.py
@@ -286,7 +286,7 @@ stormer = Stormer(img_size=[128, 256],
                   variables=variables,
                   met_weights=met_weights_with_offset,
                   patch_size=2,
-                  hidden_size=306,
+                  hidden_size=612,
                   depth=12,
                   num_heads=16,
                   mlp_ratio=4.0)
@@ -294,7 +294,7 @@ stormer = Stormer(img_size=[128, 256],
 stormer.to(device)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(stormer.parameters(),lr=0.001, betas=(0.9, 0.999), weight_decay=1e-2)
+optimizer = torch.optim.AdamW(stormer.parameters(),lr=0.00001, betas=(0.9, 0.999), weight_decay=1e-2)
 
 writer = SummaryWriter()
 
@@ -306,10 +306,10 @@ stormer_results = train(model=stormer,
                 loss_fn=loss_fn,
                 epochs=50,
                 writer=create_writer(experiment_name="stormer_runs",
-                                    extra="epochs_50_lr_1e-3_hidden_size_306_depth_12"))
+                                    extra="epochs_50_lr_1e-5_hidden_size_612_depth_12"))
 
 plot_loss_curves(stormer_results)
 
 save_model(stormer,
           "/var/scratch/ase347/DeepSummit/checkpoints",
-          "stormer_epochs_50_lr_1e-3_hidden_size_306_depth_12.pth")
+          "stormer_epochs_50_lr_1e-5_hidden_size_612_depth_12.pth")
