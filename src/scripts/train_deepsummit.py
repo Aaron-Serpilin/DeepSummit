Fixed gradient updates of fusion layer. Updated train_deepsummit.py for experiment tracking with 50 epochs, with 4 fusion layers

src/scripts/train_deepsummit.py
@@ -194,7 +194,7 @@ deepsummit = DeepSummit(
 ).to(device)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.AdamW(deepsummit.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(deepsummit.fusion.parameters(), lr=1e-3)
 
 deepsummit_results = train(model=deepsummit,
                 train_dataloader=train_dataloader,
@@ -202,11 +202,11 @@ deepsummit_results = train(model=deepsummit,
                 test_dataloader=test_dataloader,
                 optimizer=optimizer,
                 loss_fn=loss_fn,
-                epochs=10,
+                epochs=50,
                 writer=create_writer(experiment_name="deepsummit_runs",
-                                    extra="deepsummit_model_epochs_10_fusion_layers_4"))
+                                    extra="deepsummit_model_epochs_50_fusion_layers_4"))
 
 save_model(saint,
           "/var/scratch/ase347/DeepSummit/checkpoints",
-          "deepsummit_model_epochs_10_fusion_layers_4.pth")
+          "deepsummit_model_epochs_50_fusion_layers_4.pth")
 

src/tab_transformer/tab_attention.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import numpy as np
 from einops import rearrange
 
-from src.tab_transformer.tab_blocks import GEGLU, Residual, PreNorm
+from src.tab_transformer.tab_blocks import GEGLU, Residual, PreNorm, simple_MLP, MLP
 
 class FeedForward (nn.Module):
 
@@ -218,7 +218,8 @@ class RowColTransformer(nn.Module):
                  dim_head, 
                  attn_dropout, 
                  ff_dropout,
-                 style='col'):
+                 style='col'
+                 ):
         
         super().__init__()
         self.embeds = nn.Embedding(num_tokens, dim)